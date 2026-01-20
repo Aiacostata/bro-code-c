7842a0e3ed13a9d3e2ba89396cb2fd965a8b6337
Aiacostata <gtata9469@gmail.com>
2026-01-19 21:24:14 -0300

    Primeiro commit, aprendizado em C

---
+++ main.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+// preprocessor directive (input/output)
+int main() {
+
+  /*
+    B
+    U
+    C
+    É
+    F
+    A
+    L
+    O
+  */
+  printf("Bem vindo, sujeito Gustavo. Vamos fazer alguma ciência?\n");
+  return 0;
+}
