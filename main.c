c1e1241794c5f3a0a0784d4b55c887486f50276d
Aiacostata <oyaco.stav@tutamail.com>
2026-01-23 15:30:39 -0300

    Aprendi a utiizar a função scanf para ler dados direto do Terminal, como numeros inteiros, de ponto flutuante e array de caracteres. Aproveitei a oportunidade e fiz uma função básica de cálculo de IMC, sofri em relembrar aritmética básica e o resultado foi confuso no inicio, mas finalmente cai na real do problema do meu código

--- main.c
+++ main.c
@@ -1,33 +1,35 @@
 #include <stdio.h>
+#include <string.h>
+// string.h? Acho que os tópicos estão avançando rápido, isso é bom.
+
+double imc(float a, float b) { return b / (a * a); }
+// Eu estava com dificuldade de realizar essa simples tarefa pois estava pondo b
+// / a * a, ou (b / a * a), que foi a correção pois eu achava que só o valor de
+// b estava sendo impresso... bom, devo voltar a estudar aritmética básica antes
+// de programar? a resposta correta é b / (a * a) pois devo seguir sempre a
+// ordem dos operadores e sempre prestar atenção nisso,eu deveria ser um
+// estudante de exatas, porra.
 
 int main() {
-  // Operadores Aritméticos = + - * / % ++ --
 
-  int x = 20;
-  int y = 6;
-  int z = 0;
-  int a = 0;
+  int idade = 0;
+  float peso = 0.0f;
+  float altura = 0.0f;
+  char name[30] = "\0";
+  printf("Bem vindo ao laboratório da ciência, vamos fazer um teste simples, "
+         "vamos medir o seu IMC.\nQual é o seu nome? Por favor "
+         "digite: ");
+  fgets(name, sizeof(name), stdin);
+  name[strlen(name) - 1] = '\0';
 
-  int b = 0;
+  printf("Olá %s, vamos fazer ciência hoje?\n", name);
+  printf("Por favor, insira a sua idade: ");
+  scanf("%d", &idade);
+  printf("Agora insira o seu peso: ");
+  scanf("%f", &peso);
+  printf("Agora insira a sua altura: ");
+  scanf("%f", &altura);
 
-  // z = x + y;
-  // z = x - y;
-  // z = y - x;
-  // z = x * y;
-  // w = x / y;
-
-  // a = x / y;
-  // z = x % y;
-  //  b++;
-  //++b;
-  //  b--;
-  --b;
-
-  z += x;
-
-  printf("%d\n", z);
-  // printf("%d\n", a);
-
-  printf("%d\n", b);
+  printf("Seu IMC é de: %2.2lf\n", imc(altura, peso));
   return 0;
 }
